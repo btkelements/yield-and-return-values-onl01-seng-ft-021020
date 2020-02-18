b0929eff1d35fe84976d2bba367bb15297c76524
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-18 20:37:17 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def hello(array)
   i = 0
   while i < array.length
@@ -8,6 +6,3 @@ def hello(array)
     i += 1
   end
 end
-
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
